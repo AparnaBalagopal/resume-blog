5f0cf4784f0c25913b6d58f0e6b0afedcba35e12
admin <aparnabalagopal328@gmail.com>
2016-07-14 17:44:29 +0530

    edit site

--- biodata/urls.py
+++ biodata/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.first_page, name='first_page'),
      url(r'^post/(?P<pk>\d+)/$', views.second_page, name='second_page'),
+      url(r'^post/new/$', views.post_new, name='post_new'),
+      url(r'^post/(?P<pk>\d+)/edit/$', views.page_edit, name='page_edit'),
 ]
